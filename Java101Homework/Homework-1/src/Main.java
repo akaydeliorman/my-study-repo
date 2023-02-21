cea2a86cb3087defaee2bac6a5a0e143859d4d9b
akaydeliorman <dlrmn.akay@gmail.com>
2023-02-21 23:27:02 +0300

    Vücut Kitle İndeksi | Hesaplanması yapıldı.

--- Java101Homework/Homework-1/src/Main.java
+++ Java101Homework/Homework-1/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+
+    /*Kilo (kg) / Boy(m) * Boy(m)
+    Lütfen boyunuzu (metre cinsinde) giriniz : 1,72
+    Lütfen kilonuzu giriniz : 105
+    Vücut Kitle İndeksiniz : 35.49215792320173*/
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu metre cinsinden giriniz:");
+        double height = input.nextDouble();
+        System.out.print("Lütfen ağırlığınızı giriniz:");
+        double weight = input.nextDouble();
+
+        double vki = (weight/(height*height));
+        System.out.print("Vücut kitle indeksiniz: "+ vki);
+
     }
 }
